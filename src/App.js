3f08e3ff5a536fc6937326737fceab6ee55ac8b2
sungmin kang <nine.sungmin@gmail.com>
2020-02-21 15:34:05 +0900

    Update ignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, useEffect, useState} from 'react';
 import './App.css';
 
+// 생활코딩 React-class-function style study code
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var [funcShow ,setFuncShow] = useState(true);
+    var[classShow ,setClassShow] = useState(true);
+
+
+    return (
+        <div className="container">
+            <h1> Study Functional source </h1>
+            <input type="button" value="Func Remove" onClick={()=>{setFuncShow(false)}}/>
+            <input type="button" value="Class Remove"  onClick={()=>{setClassShow(false)}}/>
+            {funcShow && <FuncComp initNumber = {2}></FuncComp>}
+            {classShow && <ClassComp initNumber = {2}></ClassComp>}
+        </div>
+    );
 }
+
+function FuncComp(props) {
+    let numberState = useState(props.initNumber);
+    let number = numberState[0];
+    let setNumber = numberState[1];
+
+    // let dateState = useState((new Date).toString());
+    // let date = dateState[0];
+    // let setDate = dateState[1];
+
+    //short
+    let [date,setDate] = useState((new Date).toString())
+
+    //Effect mean is side effect
+
+    //useEffect second parmeter => watch value change
+    // useEffect(()=>{
+    //     console.log('%cFunction Render => DidMount&DidUpdate.','color:blue');
+    //
+    //     document.title = number;
+    //     return ()=>{
+    //         console.log('%cFunction Cleanup!','color:blue');
+    //     }
+    // },[number]);
+
+    //useEffect did mount
+    //just Execute 1
+    useEffect(()=>{
+        console.log('%cFunction Render => DidMount','color:blue');
+        document.title = number;
+        //if unMount = true => excute
+        return ()=>{
+            console.log('%cFunction Cleanup!','color:blue');
+        }
+    },[]);
+
+    console.log('%cFunction Render','color:blue');
+    return(
+        <div className="container">
+            <h2>func Style Component</h2>
+            <p>Number : {number}</p>
+            <p>Date : {date}</p>
+            <input type="button" value="random" onClick={()=>setNumber(Math.random())}/>
+            <input type="button" value="date" onClick={()=>setDate((new Date).toString())}/>
+        </div>
+    );
+}
+
+class ClassComp extends Component{
+    state = {
+        number:this.props.initNumber,
+        date : (new Date()).toString()
+    };
+
+    componentDidMount() {
+        console.log('%cClass Render => DidMount!','color:red');
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+    }
+
+
+    render() {
+        console.log('%cClass Render','color:red');
+        return (
+            <div className="container">
+                <h2>Class Style Component</h2>
+                <p>Number : {this.state.number}</p>
+                <p>{this.state.date}</p>
+                <input type="button" value="random" onClick={()=>this.setState({number:Math.random()})}/>
+                <input type="button" value="date" onClick={()=>this.setState({date:(new Date()).toString()})}/>
+            </div>
+        );
+    }
+}
+
 
 export default App;
